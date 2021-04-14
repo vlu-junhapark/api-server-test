2d185c022c270af33779790e0b7e0727ad1f284a
junhapark <junha.park@valuelinku.com>
2021-04-15 08:42:07 +0900

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ValueLinkU API Test Server' });
+});
+
+module.exports = router;
